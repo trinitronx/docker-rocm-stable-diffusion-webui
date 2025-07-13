6f64d4bb90aca703c5a1f29f541fb0305ed61f0d
James Cuzella <james.cuzella@lyraphase.com>
2025-07-13 01:14:58 -0600

    Speed up docker container stop using SIGKILL
    Note: Python app did not respond to SIGINT or SIGHUP properly

--- bin/run.sh
+++ bin/run.sh
@@ -4,4 +4,5 @@ docker run -it -p 7860:7860/tcp --cap-add=SYS_PTRACE --security-opt seccomp=unco
            -d --device=/dev/kfd --device=/dev/dri --group-add video \
            -v $(pwd)/data:/data:rw -e HF_TOKEN \
            --mount 'type=volume,src=hf-model-cache,dst=/var/lib/jenkins/.cache/huggingface/hub' \
+           --stop-signal SIGKILL \
            --ipc=host --shm-size 8G --rm trinitronx/rocm-stable-diffusion-webui:latest
